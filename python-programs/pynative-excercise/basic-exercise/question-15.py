1921dafe0082604d5799437cec95243bf09f833b
Nitesh Wayafalkar <niteshwayafalkar@gmail.com>
2020-11-16 09:33:32 +0530

    added basic exercise folder

--- python-programs/pynative-excercise/basic-exercise/question-15.py
+++ python-programs/pynative-excercise/basic-exercise/question-15.py
@@ -2,13 +2,15 @@
 # returns an int value of base raises to the power of exp.
 class question_15:
 
-
-    def power_of_exp(self,base,exp):
+    def power_of_exp(self, base, exp):
         result = 1
         while exp > 0:
             result = result * base
             exp -= 1
-        print(base, "raises to the power of ",exp,"is : ",result)
+        print(base, "raises to the power of ", exp, "is : ", result)
+
 
 obj = question_15()
-obj.power_of_exp(2,5)
+base = int(input("Enter base:   "))
+exp = int(input("Enter exp: "))
+obj.power_of_exp(base, exp)
---
+++ python-programs/pynative-excercise/loop-excercise/question-1.py
@@ -0,0 +1,11 @@
+# Exercise Question 1: Print First 10 natural numbers using while loop
+class loop_exercise:
+
+    def first_10_natural_numbers(self):
+        for num in range(0,11):
+            print(num)
+
+
+
+obj = loop_exercise()
+obj.first_10_natural_numbers()
